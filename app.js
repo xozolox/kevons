60159c26ca9993d6278c430c2ed629bb2db20040
xozolox <xozolho@gmail.com>
2020-11-08 19:59:26 -0300

    test

--- app.js
+++ app.js
@@ -1,100 +1,37 @@
-'use strict';
-
-const hapi = require('@hapi/hapi');
+const koa = require('koa');
+const koaRouter = require('@koa/router');
+const render = require('koa-ejs');
+const static = require('koa-static');
 const path = require('path');
-const inert = require('@hapi/inert');
-const { request } = require('http');
 
-const start = async () => {
-  const server = hapi.server({
-    port: process.env.PORT || 3000,
-    host: 'localhost',
-    routes: {
-      files: {
-        relativeTo: path.join(__dirname, 'public')
-      }
-    }
-  });
+const app = new koa();
+const router = new koaRouter();
 
-  server.route({
-    method: 'GET',
-    path: '/home/{hello}',
-    handler: (req, h) => {
-      const url = req.params.hello;
-      return `HELLO WORLD! ${url}`;
-    }
-  })
-
-  server.route({
-    method: 'POST',
-    path: '/home',
-    handler: (req, h) => {
-      const url = req.payload.hello;
-      return `Hello World! ${url}`;
-    }
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (req, h) => {
-      return  h.view('main')
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/products',
-    handler: (req, h) => {
-      return h.view('product', {
-        number: [
-          {title: 'PARKA DE CUERO', categoria: 'Parkas', talle: 45, color: 'negro oscuro'}, 
-          {title: 'JEANS DE CUERO', categoria: 'Jeans', talle: 36, color: 'celeste degradado'}, 
-          {title: 'CAMISAS DE RAYAS', categoria: 'Camisas', talle: 38, color: 'rojo carmesi'},
-          {title: 'PANTALONES XXXL', categoria: 'Pantalones', talle: 67, color: 'amarillo'}
-        ],
-      });
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/contactanos',
-    handler: (req, h) => {
-      return h.view('contactanos');
-    }
-  });
-
-  await server.register(inert);
-
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: '.'
-      }
-    }
-  })
-
-  await server.register(require('@hapi/vision'));
-
-  server.views({
-    engines: {
-      html: require('ejs'),
-    },
-    relativeTo: __dirname,
-    path: './views',
-    isCached: false
-  });
-
-  await server.start();
-  console.log(`Sever init in port: ${server.info.uri}`);
-};
-
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: false,
+  viewExt: 'ejs',
+  cache: false,
+  debug: false,
 });
 
-start();
+router
+  .get('/', async ctx => await ctx.render('main', {
+    title: 'KEVONS',
+  }))
+  .get('/products', ctx => ctx.render('product', {
+    number: [
+      {title: 'PARKA DE CUERO', categoria: 'Parkas', talle: 45, color: 'negro oscuro'}, 
+      {title: 'JEANS DE CUERO', categoria: 'Jeans', talle: 36, color: 'celeste degradado'}, 
+      {title: 'CAMISAS DE RAYAS', categoria: 'Camisas', talle: 38, color: 'rojo carmesi'},
+      {title: 'PANTALONES XXXL', categoria: 'Pantalones', talle: 67, color: 'amarillo'}
+    ],
+  }))
+  .get('/contactanos', ctx => ctx.render('contactanos'));
+
+app.use(static(path.join(__dirname, 'public')));
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+app.listen({port: process.env.PORT || 3000});
