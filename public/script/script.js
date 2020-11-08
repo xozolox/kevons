5c90e6f14af5c9bcfa6c5dcff79cc8427918544f
xozolox <xozolho@gmail.com>
2020-11-08 20:05:38 -0300

    heroku

--- public/script/script.js
+++ public/script/script.js
@@ -18,9 +18,9 @@ let test = lista.filter(num => num == 2);
 
 //PRUEBA
 
-//document.querySelector('.item-image').src = 'https://images.unsplash.com/photo-1603211234763-b279ec387d64?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
-//document.querySelector('.item-images').src = 'https://images.unsplash.com/photo-1603931690344-d546e4928b99?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
-//document.querySelector('.item-imagese').src = 'https://images.unsplash.com/photo-1603778252234-162da7e262ba?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
-//document.querySelector('.item-img').src = 'https://images.unsplash.com/photo-1603363586831-a8477630fc19?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
-//document.querySelector('.item-imgs').src = 'https://images.unsplash.com/photo-1603458360961-8878b5ce18f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
-//document.querySelector('.item-imgse').src = 'https://images.unsplash.com/photo-1602185505373-f5130988677b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-image').src = 'https://images.unsplash.com/photo-1603211234763-b279ec387d64?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-images').src = 'https://images.unsplash.com/photo-1603931690344-d546e4928b99?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-imagese').src = 'https://images.unsplash.com/photo-1603778252234-162da7e262ba?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-img').src = 'https://images.unsplash.com/photo-1603363586831-a8477630fc19?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-imgs').src = 'https://images.unsplash.com/photo-1603458360961-8878b5ce18f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
+document.querySelector('.item-imgse').src = 'https://images.unsplash.com/photo-1602185505373-f5130988677b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max';
